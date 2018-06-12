e22ce2da73de6f0878013833bca86ef87b51c526
noobsmonster <12567356+blueskypig@users.noreply.github.com>
2018-06-12 07:13:12 -0700

    update

--- code/Spark_GBTC.py
+++ code/Spark_GBTC.py
@@ -7,20 +7,20 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession
 
-
+# Read data
 spark = SparkSession.builder.appName("pyspark data source").getOrCreate()
 df = spark.read.load("../data/training.csv", format="csv", header="true")
 test_df = spark.read.load("../data/testing.csv", format="csv", header="true")
 
+# Convert DataType and DataFrame
 features_name = [f for f in df.columns if f not in ['TARGET', 'SK_ID_CURR', 'SK_ID_BUREAU', 'SK_ID_PREV', 'index']]
-
 for name in features_name:
     df = df.withColumn(name, df[name].cast("double"))
     df_test = df_test.withColumn(name, df[name].cast("double"))
-
 df = df.withColumn("indexedLabel", df["SK_ID_CURR"].cast("double"))
 df_test = df_test.withColumn("indexedLabel", df["SK_ID_CURR"].cast("double"))
 
+# Assemble features as sparse Vector
 assembler = VectorAssembler(inputCols=features_name, outputCol= "features")
 features = assembler.transform(df)
 df = df.withColumn("indexedFeatures", features)
@@ -32,8 +32,7 @@ del df, df_test
 # Train a GBT model.
 gbt = GBTClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", maxIter=10)
 
-
-# Train model.  This also runs the indexers.
+# Train model.
 model = gbt.fit(trainingData)
 
 # Make predictions.
